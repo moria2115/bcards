fe783291f8e5b73d1bcdebd06dcbe7e584c3c94b
moria2115 <moria2115@gmail.com>
2023-01-29 21:21:19 +0200

    Update Profile.tsx
    update profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,26 +5,32 @@ import { getUserById } from "../services/usersService";
 interface ProfileProps {}
 
 const Profile: FunctionComponent<ProfileProps> = () => {
-  let [user, setUser] = useState<User>({
-    name: "",
-    email: "",
-    password: "",
-  });
-  let userId: number = JSON.parse(
-    sessionStorage.getItem("userData") as string
-  ).userId;
+  let [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    getUserById(userId)
-      .then((res) => {
-        setUser(res.data);
-      })
-      .catch((err) => console.log(err));
+    const getUser = async () => {
+      try {
+        let userId: number = await JSON.parse(
+          sessionStorage.getItem("userData") as string
+        ).userId;
+        getUserById(userId)
+          .then((res) => {
+            setUser(res.data);
+          })
+          .catch((err) => console.log(err));
+      } catch (error) {
+        console.log(error);
+      }
+      getUser();
+    };
   }, []);
+
   return (
     <>
       <h1 className="display-1 text-center">My Profile</h1>
-      <h3 className="display-3 text-center">Hello, {user.name}</h3>
+      <h3 className="display-3 text-center">
+        Hello, {user?.name ?? "Stranger"}
+      </h3>
     </>
   );
 };
