1fcfd4323da3cb8fd24a6cf65e66a9100d2aa933
moria2115 <moria2115@gmail.com>
2023-02-01 11:32:51 +0200

    all cards + home

--- src/components/AllCards.tsx
+++ src/components/AllCards.tsx
@@ -3,6 +3,7 @@ import Card from "../interfaces/Card";
 import { getAllCards } from "../services/cardsService";
 import { successMsg } from "../services/feebacks";
 import { addCardToFavorites } from "../services/usersService";
+// import "../css/cards.css";
 
 interface AllCardsProps {}
 
@@ -17,41 +18,51 @@ const AllCards: FunctionComponent<AllCardsProps> = () => {
 
   return (
     <>
+      <h1 className="display-1 text-center my-3">
+        <i className="fa-solid fa-address-card"></i> BUSINESS CARDS
+      </h1>
       {cards.length ? (
-        <div className="container">
+        <div className="container mx-auto">
           <div className="row">
             {cards.map((card: Card) => (
               <div
-                key={card.id}
-                className="card ms-1"
+                className="card border mx-auto"
                 style={{ width: "18rem" }}
+                key={card.id}
               >
                 <img
                   src={card.image}
                   className="card-img-top"
                   alt={card.name}
-                  style={{ height: "100%" }}
+                  style={{ height: "10rem" }}
                 />
                 <div className="card-body">
                   <h5 className="card-title">{card.name}</h5>
                   <p className="card-text">{card.description}</p>
-                  <p className="text-success">{card.phone}</p>
-                  <p className="text-success">{card.address}</p>
-                  <p className="text-success">{card.website}</p>
-
-                  <button
-                    className="btn btn-outline-warning  w-100 "
-                    onClick={() => {
-                      addCardToFavorites(card.id as number)
-                        .then(() =>
-                          successMsg(`Card ${card.name} added to favorites`)
-                        )
-                        .catch((err) => console.log(err));
-                    }}
-                  >
-                    <i className="fa-regular fa-heart"></i>
-                  </button>
                 </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">
+                    <i className="fa-solid fa-phone"></i> {card.phone}
+                  </li>
+                  <li className="list-group-item">
+                    <i className="fa-solid fa-location-pin"></i> {card.address}
+                  </li>
+                  <li className="list-group-item">
+                    <i className="fa-solid fa-globe"></i> {card.website}
+                  </li>
+                </ul>
+                <button
+                  className="btn btn-outline-warning my-2 w-100 "
+                  onClick={() => {
+                    addCardToFavorites(card.id as number)
+                      .then(() =>
+                        successMsg(`Card ${card.name} added to favorites`)
+                      )
+                      .catch((err) => console.log(err));
+                  }}
+                >
+                  <i className="fa-regular fa-heart"></i>
+                </button>
               </div>
             ))}
           </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 import "../css/home.css";
 
 interface HomeProps {}
@@ -8,6 +9,22 @@ const Home: FunctionComponent<HomeProps> = () => {
     <>
       <div className="container">
         <h1 className=" text-center display-1">WELCOME TO BCARDS</h1>
+        <div className="row">
+          <div className="col-md-7 my-auto">
+            <h3 className="display-3">Create your own business card easily</h3>
+            <Link className="btn btn-primary mx-2 mt-3" to={"/signIn"}>
+              Sign In
+            </Link>
+            <Link className="btn btn-primary mt-3" to={"/signUp"}>
+              Sign Up
+            </Link>
+          </div>
+          <img
+            className="col-md-5"
+            src="\images\Tech_Tech_Website_in_Teal_White_Navy_Gradients_Style__1_-removebg-preview.png"
+            alt="Mobile image"
+          />
+        </div>
       </div>
     </>
   );
